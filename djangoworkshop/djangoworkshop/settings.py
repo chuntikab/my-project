3b381bc771aac0db0103ea9452a4016ab4d40403
chuntikab <chuntika.bum@gmail.com>
2021-03-10 05:51:23 +0700

    แสดงจำนวนสินค้าในตะกร้า

--- djangoworkshop/djangoworkshop/settings.py
+++ djangoworkshop/djangoworkshop/settings.py
@@ -71,7 +71,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.context_processors.menu_links'
+                'store.context_processors.menu_links',
+                'store.context_processors.counter'
             ],
         },
     },
--- djangoworkshop/store/context_processors.py
+++ djangoworkshop/store/context_processors.py
@@ -1,5 +1,22 @@
-from store.models import Category
+from store.models import Category,CartItem,Cart
+from store.views import _cart_id
 
 def menu_links(request):
     links=Category.objects.all()
     return dict(links=links)
+
+def counter(request):
+    item_count=0
+    if 'admin' in request.path: # เช็คผ่าน path
+        return {}
+    else:
+        try:    
+            # query cart
+            cart=Cart.objects.filter(cart_id=_cart_id(request)) 
+            # query cartitem
+            cart_Item=CartItem.objects.all().filter(cart=cart[:1]) # เก็บผลลัพที่ได้จากการดึงฐานข้อมูลรายการสินค้าในตะกร้า  / :1 โยน รหัสสินค้าเข้าไป
+            for item in cart_Item:
+                item_count+=item.quantity
+        except Cart.DoesNotExist:
+            item_count=0
+    return dict(item_count=item_count)
