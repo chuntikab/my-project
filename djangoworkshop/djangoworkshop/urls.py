938b7092d55a6f1ebda396b980dd9b5cb11a9fd8
chuntikab <chuntika.bum@gmail.com>
2021-03-10 04:33:52 +0700

    แก้ url pattern ให้กด cart ได้หลายครั้ง, ใส่ชื่อเว็บและแก้โลโก้

--- djangoworkshop/djangoworkshop/urls.py
+++ djangoworkshop/djangoworkshop/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('category/<slug:category_slug>',views.index,name="product_by_category"),
     path('product/<slug:category_slug>/<slug:product_slug>',views.productPage,name='productDetail'),
     path('cart/add/<int:product_id>',views.addCart,name="addCart"),
-    path('cartdetail/',views.cartdetail)
+    path('cartdetail/',views.cartdetail,name="cartdetail")
 ]
 
 #product/fashion/shoes
